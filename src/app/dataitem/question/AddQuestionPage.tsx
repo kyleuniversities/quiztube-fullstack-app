e085765dd3fdf1e97c46b47aa06b2ae6fb08d1e7
kyleuniversities <kyleuniversities@gmail.com>
2023-12-17 03:07:29 -0800

    impored quizzical-api-app-beta

--- src/app/dataitem/question/AddQuestionPage.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router';
-import { AddModifyQuestionPage } from './AddModifyQuestionPage';
-
-export const AddQuestionPage = (): JSX.Element => {
-  const { quizId } = useParams();
-  return <AddModifyQuestionPage quizId={quizId} id={undefined} />;
-};
