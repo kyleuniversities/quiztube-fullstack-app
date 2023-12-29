5f5ffb6a8ae667a6199eb251385bbfd270a6c9d8
kyleuniversities <kyleuniversities@gmail.com>
2023-12-28 20:28:34 -0800

    set up s3 services stub

---
+++ backend/src/main/java/com/ku/quizzical/app/s3/S3StorageBucketsModel.java
@@ -0,0 +1,21 @@
+package com.ku.quizzical.app.s3;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "aws.s3.buckets")
+public class S3StorageBucketsModel {
+    // Instance Fields
+    private String master;
+
+    // Accessor Methods
+    public String getMaster() {
+        return this.master;
+    }
+
+    // Mutator Methods
+    public void setMaster(String master) {
+        this.master = master;
+    }
+}
