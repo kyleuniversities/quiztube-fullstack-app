f0c4f304da0f36ad86edc4935c983215c4cdb7ff
kyleuniversities <kyleuniversities@gmail.com>
2023-12-28 16:46:18 -0800

    reformatted UserRowMapper

--- backend/src/main/java/com/ku/quizzical/app/controller/user/UserRowMapper.java
+++ backend/src/main/java/com/ku/quizzical/app/controller/user/UserRowMapper.java
@@ -9,29 +9,28 @@ import org.springframework.stereotype.Component;
 import com.ku.quizzical.app.controller.comment.Comment;
 import com.ku.quizzical.app.controller.like.Like;
 import com.ku.quizzical.app.controller.quiz.Quiz;
-import com.ku.quizzical.app.controller.quiz.QuizService;
 import com.ku.quizzical.common.helper.ListHelper;
 
 @Component
 public class UserRowMapper implements RowMapper<User> {
-    private UserRepository userRepository;
+        private UserRepository userRepository;
 
-    public UserRowMapper(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+        public UserRowMapper(UserRepository userRepository) {
+                this.userRepository = userRepository;
+        }
 
-    @Override
-    public User mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
-        Optional<User> userQuery = this.userRepository.findById(resultSet.getString("id"));
-        List<Quiz> quizzes =
-                userQuery.isEmpty() ? ListHelper.newArrayList() : userQuery.get().getQuizzes();
-        List<Like> likes =
-                userQuery.isEmpty() ? ListHelper.newArrayList() : userQuery.get().getLikes();
-        List<Comment> comments =
-                userQuery.isEmpty() ? ListHelper.newArrayList() : userQuery.get().getComments();
-        return new User(resultSet.getString("id"), resultSet.getString("username"),
-                resultSet.getString("email"), resultSet.getString("password"),
-                resultSet.getString("profile_picture"), resultSet.getString("thumbnail"), quizzes,
-                likes, comments);
-    }
+        @Override
+        public User mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
+                Optional<User> userQuery = this.userRepository.findById(resultSet.getString("id"));
+                List<Quiz> quizzes = userQuery.isEmpty() ? ListHelper.newArrayList()
+                                : userQuery.get().getQuizzes();
+                List<Like> likes = userQuery.isEmpty() ? ListHelper.newArrayList()
+                                : userQuery.get().getLikes();
+                List<Comment> comments = userQuery.isEmpty() ? ListHelper.newArrayList()
+                                : userQuery.get().getComments();
+                return new User(resultSet.getString("id"), resultSet.getString("username"),
+                                resultSet.getString("email"), resultSet.getString("password"),
+                                resultSet.getString("profile_picture"),
+                                resultSet.getString("thumbnail"), quizzes, likes, comments);
+        }
 }
