8f6cc589496e919964b785f295aa67038c89e9f2
kyleuniversities <kyleuniversities@gmail.com>
2023-12-28 17:16:20 -0800

    removed optionals from subject service interface

--- backend/src/main/java/com/ku/quizzical/app/controller/subject/SubjectDatabaseService.java
+++ backend/src/main/java/com/ku/quizzical/app/controller/subject/SubjectDatabaseService.java
@@ -1,16 +1,15 @@
 package com.ku.quizzical.app.controller.subject;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface SubjectService {
+public interface SubjectDatabaseService {
     void saveSubject(SubjectDto subject);
 
     List<SubjectDto> getAllSubjects();
 
-    Optional<SubjectDto> getSubject(String id);
+    SubjectDto getSubject(String id);
 
-    Optional<SubjectDto> getSubjectByText(String text);
+    SubjectDto getSubjectByText(String text);
 
     void updateSubject(String id, SubjectDto subject);
 
