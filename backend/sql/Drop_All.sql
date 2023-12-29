0d9ad5e761385a2787e297be8772c39b73ba1d51
kyleuniversities <kyleuniversities@gmail.com>
2023-12-28 19:32:30 -0800

    added drop all sql query

---
+++ backend/sql/Drop_All.sql
@@ -0,0 +1,6 @@
+DROP TABLE development._like;
+DROP TABLE development.comment;
+DROP TABLE development.question;
+DROP TABLE development.quiz;
+DROP TABLE development.subject;
+DROP TABLE development.user;
