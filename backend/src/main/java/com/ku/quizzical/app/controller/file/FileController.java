c2594cc028706d76b5d4f827394af9110addabad
kyleuniversities <kyleuniversities@gmail.com>
2023-12-30 05:51:10 -0800

    cleaned up file ctrl

--- backend/src/main/java/com/ku/quizzical/app/controller/file/FileController.java
+++ backend/src/main/java/com/ku/quizzical/app/controller/file/FileController.java
@@ -1,24 +1,13 @@
 package com.ku.quizzical.app.controller.file;
 
-import java.util.List;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.ku.quizzical.app.controller.auth.AuthenticationRequest;
-import com.ku.quizzical.app.controller.auth.AuthenticationResponse;
-import com.ku.quizzical.app.controller.auth.AuthenticationService;
 import com.ku.quizzical.app.s3.S3StorageService;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.ku.quizzical.common.helper.string.StringReplacementHelper;
 
 @CrossOrigin
 @RestController
@@ -36,6 +25,7 @@ public final class FileController {
     // Downloads an image file
     @GetMapping(value = "/image/{key}", produces = MediaType.IMAGE_PNG_VALUE)
     public byte[] getFileImage(@PathVariable String key) {
-        return this.s3StorageService.getObject(key);
+        String decodedKey = StringReplacementHelper.replace(key, "__", "/");
+        return this.s3StorageService.getObject(decodedKey);
     }
 }
