6cdb5d6cf1b55443c9fff302a89cf87f88348d7b
kyleuniversities <kyleuniversities@gmail.com>
2023-12-30 02:56:04 -0800

    setup bcrypt synth password encoding

--- backend/src/main/java/com/ku/quizzical/devtools/util/synthetic/SyntheticUserDataGenerator.java
+++ backend/src/main/java/com/ku/quizzical/devtools/util/synthetic/SyntheticUserDataGenerator.java
@@ -1,9 +1,13 @@
 package com.ku.quizzical.devtools.util.synthetic;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import com.ku.quizzical.common.helper.ListHelper;
 import com.ku.quizzical.common.helper.string.StringHelper;
 
 final class SyntheticUserDataGenerator extends SyntheticSubDataGenerator {
+    private PasswordEncoder encoder;
+
     // New Instance Method
     public static SyntheticUserDataGenerator newInstance(SyntheticDataGenerator generator,
             SyntheticStaticData staticData, SyntheticDataIdHolder idHolder) {
@@ -14,6 +18,7 @@ final class SyntheticUserDataGenerator extends SyntheticSubDataGenerator {
     private SyntheticUserDataGenerator(SyntheticDataGenerator generator,
             SyntheticStaticData staticData, SyntheticDataIdHolder idHolder) {
         super(generator, staticData, idHolder);
+        this.encoder = new BCryptPasswordEncoder();
     }
 
     // Accessor Methods
@@ -31,10 +36,12 @@ final class SyntheticUserDataGenerator extends SyntheticSubDataGenerator {
         ListHelper.forEach(this.staticData.getUsernames(), (Integer i, String username) -> {
             String id = this.idHolder.getUserIds().get(i);
             String firstLetter = username.charAt(0) + "";
+            String rawPassword = username + "!";
+            String password = this.encoder.encode(rawPassword);
             String pictureText = "static/user/user-picture-" + firstLetter + ".png";
             String thumbnailText = "static/user/user-picture-" + firstLetter + "_T.png";
             String recordText = StringHelper.format("('%s', '%s', '%s', '%s', '%s', '%s'),", id,
-                    username, username + "@gamil.com", username + "!", pictureText, thumbnailText);
+                    username, username + "@gamil.com", password, pictureText, thumbnailText);
             this.addLine(recordText);
         });
         this.deleteLastCharacters(1);
