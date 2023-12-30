9d9362200fefd8d3cca35e77742433614811ce6c
kyleuniversities <kyleuniversities@gmail.com>
2023-12-30 02:56:20 -0800

    generated encoded synth data

--- backend/src/main/resources/synthetic-data/output/subject.sql
+++ backend/src/main/resources/synthetic-data/output/subject.sql
@@ -1,7 +1,7 @@
 INSERT INTO development.subject (id, text, picture, thumbnail)
 VALUES
-('3db92f4c-78ea-448e307e', 'Arts', 'static/subject/subject-picture-Arts.png', 'static/subject/subject-picture-Arts.png'),
-('5b924b78-e44d-83aece07', 'English', 'static/subject/subject-picture-English.png', 'static/subject/subject-picture-English.png'),
-('79ed247c-c844-d8ae307c', 'Math', 'static/subject/subject-picture-Math.png', 'static/subject/subject-picture-Math.png'),
-('e0f0c3de-e478-e448307e', 'Science', 'static/subject/subject-picture-Science.png', 'static/subject/subject-picture-Science.png'),
-('2b0347f8-e44e-8afff307', 'Social Studies', 'static/subject/subject-picture-Social Studies.png', 'static/subject/subject-picture-Social Studies.png');
+('11c5c794-dcf8-0c9a307d', 'Arts', 'static/subject/subject-picture-Arts.png', 'static/subject/subject-picture-Arts_T.png'),
+('3e15b79f-ecce-4a809307', 'English', 'static/subject/subject-picture-English.png', 'static/subject/subject-picture-English_T.png'),
+('dc6eb15f-7948-09a3fa07', 'Math', 'static/subject/subject-picture-Math.png', 'static/subject/subject-picture-Math_T.png'),
+('b71579fb-4e8b-0d9af307', 'Science', 'static/subject/subject-picture-Science.png', 'static/subject/subject-picture-Science_T.png'),
+('915edae7-cbaa-94809307', 'Social Studies', 'static/subject/subject-picture-Social Studies.png', 'static/subject/subject-picture-Social Studies_T.png');
