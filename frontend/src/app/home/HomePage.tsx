6646c455eb4b8a218d48767ec406111476ec723f
kyleuniversities <kyleuniversities@gmail.com>
2023-12-30 13:31:36 -0800

    added empty quiz text

--- frontend/src/app/home/HomePage.tsx
+++ frontend/src/app/home/HomePage.tsx
@@ -2,7 +2,6 @@ import { Header } from 'semantic-ui-react';
 import { ConditionalContent } from '../ConditionalContent';
 import { SitePage } from '../SitePage';
 import { NULL_ID, useUserId } from '../context/AppContextManager';
-import { ViewListDataItemsPage } from '../dataitem/ViewListDataItemsPage';
 import { ViewAllQuizzesPage } from '../dataitem/quiz/ViewAllQuizzesPage';
 
 /**
