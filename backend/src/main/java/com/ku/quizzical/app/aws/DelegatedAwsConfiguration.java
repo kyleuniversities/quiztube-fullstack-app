c3db6572491e15418b61c2fe871a6268d02104b4
kyleuniversities <kyleuniversities@gmail.com>
2024-01-01 05:39:56 -0800

    set up aws credentials

---
+++ backend/src/main/java/com/ku/quizzical/app/aws/DelegatedAwsConfiguration.java
@@ -0,0 +1,22 @@
+package com.ku.quizzical.app.aws;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DelegatedAwsConfiguration {
+    // Instance Fields
+    @Value("${aws.credentials.access-key-id}")
+    private String accessKeyId;
+
+    @Value("${aws.credentials.secret-access-key}")
+    private String secretAccessKey;
+
+    public String getAccessKeyId() {
+        return this.accessKeyId;
+    }
+
+    public String getSecretAccessKey() {
+        return this.secretAccessKey;
+    }
+}
--- backend/src/main/java/com/ku/quizzical/app/s3/S3StorageConfiguration.java
+++ backend/src/main/java/com/ku/quizzical/app/s3/S3StorageConfiguration.java
@@ -3,6 +3,7 @@ package com.ku.quizzical.app.s3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.ku.quizzical.app.aws.DelegatedAwsService;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -12,21 +13,24 @@ public class S3StorageConfiguration {
     @Value("${aws.region}")
     private String awsRegion;
 
-    @Value("${aws.s3.bucketpath}")
+    @Value("${aws.s3.bucket-path}")
     private String bucketPath;
+
+    private DelegatedAwsService awsService;
 
     public String getBucketPath() {
         return this.bucketPath;
     }
 
+    public S3StorageConfiguration(DelegatedAwsService awsService) {
+        this.awsService = awsService;
+    }
+
     @Bean
     public S3Client s3Client() {
         if (this.bucketPath.contains("http")) {
-            return S3Client.builder()
-                    .region(Region.of(this.awsRegion))
-                    // TODO (Later): Configure credentials later
-                    // .credentialsProvider(AwsCredentialsProvider + AwsCredentials)
-                    .build();
+            return S3Client.builder().region(Region.of(this.awsRegion))
+                    .credentialsProvider(this.awsService).build();
         }
         return new FakeS3Client(this.bucketPath);
     }
