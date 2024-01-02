7173d2c83d102f9304b9372d3d37402defe496ee
kyleuniversities <kyleuniversities@gmail.com>
2024-01-02 09:14:40 -0800

    fixed blank question synth data pop error

--- backend/src/main/resources/synthetic-data/output/subject.sql
+++ backend/src/main/resources/synthetic-data/output/subject.sql
@@ -1,7 +1,7 @@
 INSERT INTO development.subject (id, text, picture, thumbnail)
 VALUES
-('11c5c794-dcf8-0c9a307d', 'Arts', 'static/subject/subject-picture-Arts.png', 'static/subject/subject-picture-Arts_T.png'),
-('3e15b79f-ecce-4a809307', 'English', 'static/subject/subject-picture-English.png', 'static/subject/subject-picture-English_T.png'),
-('dc6eb15f-7948-09a3fa07', 'Math', 'static/subject/subject-picture-Math.png', 'static/subject/subject-picture-Math_T.png'),
-('b71579fb-4e8b-0d9af307', 'Science', 'static/subject/subject-picture-Science.png', 'static/subject/subject-picture-Science_T.png'),
-('915edae7-cbaa-94809307', 'Social Studies', 'static/subject/subject-picture-Social Studies.png', 'static/subject/subject-picture-Social Studies_T.png');
+('92eeea12-4209-1d4acc07', 'Arts', 'static/subject/subject-picture-Arts.png', 'static/subject/subject-picture-Arts_T.png'),
+('1d3124e2-0eaf-9e1c40b7', 'English', 'static/subject/subject-picture-English.png', 'static/subject/subject-picture-English_T.png'),
+('d3312a42-d09d-ae14c07d', 'Math', 'static/subject/subject-picture-Math.png', 'static/subject/subject-picture-Math_T.png'),
+('5312ef4d-2ffc-091f407f', 'Science', 'static/subject/subject-picture-Science.png', 'static/subject/subject-picture-Science_T.png'),
+('8d3da1c2-e4e2-ec091407', 'Social Studies', 'static/subject/subject-picture-Social Studies.png', 'static/subject/subject-picture-Social Studies_T.png');
