9f49468e0d49f7029b6818435b5bfcabc9d34d4d
kyleuniversities <kyleuniversities@gmail.com>
2024-01-01 16:31:26 -0800

    added blanket exception debugger

--- frontend/src/app/util/exception.tsx
+++ frontend/src/app/util/exception.tsx
@@ -4,6 +4,7 @@ import { debugAlert } from '../service/debug';
  * Default function for handling exceptions
  */
 export const handleException = (exception: any): void => {
+  debugAlert(`ERROR: ${JSON.stringify(exception)}`);
   debugAlert(`ERROR DATA: ${JSON.stringify(exception.response.data)}`);
   alert(`${exception.response.data.message}`);
 };
