e35797264db1de489f60af0444a825ade25b1722
kyleuniversities <kyleuniversities@gmail.com>
2024-01-03 02:39:46 -0800

    set up substr equals test

--- frontend/src/test/common/helper/string/StringHelper.test.tsx
+++ frontend/src/test/common/helper/string/StringHelper.test.tsx
@@ -2,8 +2,14 @@ import { StringHelper } from '../../../../common/helper/string/StringHelper';
 import { StringList } from '../../../../common/util/string';
 
 describe('Testing StringList methods', () => {
-  test('Makes an Empty String List', () => {
+  test('newEmptyStringList() makes an empty StringList', () => {
     const list: StringList = StringHelper.newEmptyStringList();
     expect(list.length).toBe(0);
+  });
+
+  test('substringEquals() identifies substring', () => {
+    const text: string = 'The fox walked across the street.';
+    const target: string = 'walked';
+    expect(StringHelper.substringEquals(text, target, 8)).toBe(true);
   });
 });
