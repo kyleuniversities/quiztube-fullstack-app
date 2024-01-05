f6fbceec7ca522fbb9ec8b9d0798fefa8a03ea35
kyleuniversities <kyleuniversities@gmail.com>
2024-01-05 00:58:03 -0800

    uncommented user tests

--- backend/src/test/java/com/ku/quizzical/app/controller/user/UserControllerTest.java
+++ backend/src/test/java/com/ku/quizzical/app/controller/user/UserControllerTest.java
@@ -12,8 +12,10 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class UserControllerTest {
     @LocalServerPort
     private int port;
@@ -23,25 +25,30 @@ public class UserControllerTest {
 
     @Test
     void usersGetTest() throws Exception {
-        // this.restTemplate.getForObject(this.fullUrl("/users"), List.class);
+        this.restTemplate.getForObject(this.fullUrl("/users"), List.class);
     }
 
     @Test
     void userPostTest() throws Exception {
-        /*
-         * List<UserDto> users = this.restTemplate.getForObject(this.fullUrl("/users"), List.class);
-         * System.out.println("---------------"); System.out.println("<<USERS>>");
-         * System.out.println(users); System.out.println("---------------"); int initialSize =
-         * users.size(); JSONObject body = new JSONObject(); body.put("username", "test123");
-         * body.put("email", "test123@gmail.com"); body.put("password", "test123@gmail.com");
-         * body.put("picture", "static/user/user-picture-t.png"); body.put("thumbnail",
-         * "static/user/user-picture-t_T.png"); HttpHeaders headers = new HttpHeaders();
-         * headers.setContentType(MediaType.APPLICATION_JSON); HttpEntity<String> request = new
-         * HttpEntity<String>(body.toString(), headers); UserDto userDto = this.restTemplate
-         * .postForEntity(this.fullUrl("/users"), request, UserDto.class).getBody(); List<UserDto>
-         * users2 = this.restTemplate.getForObject(this.fullUrl("/users"), List.class);
-         * assertThat(users.size() + 1).isEqualTo(users2.size());/
-         **/
+        List<UserDto> users = this.restTemplate.getForObject(this.fullUrl("/users"), List.class);
+        System.out.println("---------------");
+        System.out.println("<<USERS>>");
+        System.out.println(users);
+        System.out.println("---------------");
+        int initialSize = users.size();
+        JSONObject body = new JSONObject();
+        body.put("username", "test123");
+        body.put("email", "test123@gmail.com");
+        body.put("password", "test123@gmail.com");
+        body.put("picture", "static/user/user-picture-t.png");
+        body.put("thumbnail", "static/user/user-picture-t_T.png");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> request = new HttpEntity<String>(body.toString(), headers);
+        UserDto userDto = this.restTemplate
+                .postForEntity(this.fullUrl("/users"), request, UserDto.class).getBody();
+        List<UserDto> users2 = this.restTemplate.getForObject(this.fullUrl("/users"), List.class);
+        assertThat(users.size() + 1).isEqualTo(users2.size());
     }
 
     private String fullUrl(String url) {
